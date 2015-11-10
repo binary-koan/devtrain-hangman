0109127a4a42d3b4a5f7553dc0628ce591d0f36f
Jono Mingard <reason.koan@gmail.com>
2015-11-11 09:40:43 +1300

    Improve doubles in controller tests

--- spec/controller_spec.rb
+++ spec/controller_spec.rb
@@ -2,9 +2,18 @@ require_relative '../lib/controller'
 require_relative '../lib/hangman'
 require_relative '../lib/interface'
 
-describe Controller do
-  let(:game) { instance_double(Hangman) }
-  let(:view) { instance_double(Interface) }
+RSpec.describe Controller do
+  let(:game) {
+    instance_double(Hangman,
+      in_progress?: false,
+      apply_guess: true)
+  }
+  let(:view) {
+    instance_double(Interface,
+      print_game_state: nil,
+      print_game_result: nil,
+      ask_for_guess: nil)
+  }
   let(:controller) { Controller.new(game, view) }
 
   describe '#play' do
@@ -17,22 +26,12 @@ describe Controller do
     it 'prints the game state if the game is not finished' do
       expect(game).to receive(:in_progress?).and_return(true, false)
       expect(view).to receive(:print_game_state)
-
-      allow(view).to receive(:ask_for_guess)
-      allow(game).to receive(:apply_guess).and_return(true)
-      allow(view).to receive(:print_game_result)
-
       controller.play
     end
 
     it 'prints the game state each turn until the game is finished' do
       expect(game).to receive(:in_progress?).and_return(true, true, true, false)
       expect(view).to receive(:print_game_state).at_least(3).times
-
-      allow(view).to receive(:ask_for_guess)
-      allow(game).to receive(:apply_guess).and_return(true)
-      allow(view).to receive(:print_game_result)
-
       controller.play
     end
   end
