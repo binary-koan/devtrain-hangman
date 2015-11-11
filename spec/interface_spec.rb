4de2a64013379777ab92ac1d80b979ecbb232f1a
Jono Mingard <reason.koan@gmail.com>
2015-11-11 13:44:59 +1300

    Use instance double for Game in interface spec

--- spec/interface_spec.rb
+++ spec/interface_spec.rb
@@ -3,7 +3,14 @@ require_relative 'spec_helper'
 RSpec.describe Interface do
   include SpecHelper
 
-  let(:game) { Game.new('test') }
+  let(:incorrect_guesses) { Set.new }
+  let(:game) do
+    instance_double(Game,
+      word: "TEST",
+      guessed?: false,
+      incorrect_guesses: incorrect_guesses)
+  end
+
   let(:view) { Interface.new(game) }
 
   before do
@@ -13,23 +20,29 @@ RSpec.describe Interface do
   end
 
   describe "#print_game_state" do
-    it "initially prints the target word as underscores" do
-      expect(view).to receive(:puts).with('_' * game.word.length)
-      view.print_game_state
+    context "when no letters have been guessed" do
+      it "prints the target word as underscores" do
+        expect(view).to receive(:puts).with("____")
+        view.print_game_state
+      end
     end
 
-    it "prints the target word with letters when letters have been guessed" do
-      game.apply_guess('T')
-      game.apply_guess('E')
-      expect(view).to receive(:puts).with('TE_T')
-      view.print_game_state
+    context "when correct letters have been guessed" do
+      it "prints the target word with letters and underscores" do
+        expect(game).to receive(:guessed?).and_return(true, true, false)
+        expect(view).to receive(:puts).with("TE__")
+        view.print_game_state
+      end
     end
 
-    it "prints the number of incorrect guesses" do
-      guesses = guess_incorrectly(2)
-      expect(view).to receive(:print).with(/2 letters incorrect/)
-      expect(view).to receive(:puts).with("#{guesses[0]} #{guesses[1]}")
-      view.print_game_state
+    context "when incorrect letters have been guessed" do
+      let(:incorrect_guesses) { Set.new(["A", "B"]) }
+
+      it "prints the number of incorrect guesses" do
+        expect(view).to receive(:print).with(/2 letters incorrect/)
+        expect(view).to receive(:puts).with("A B")
+        view.print_game_state
+      end
     end
   end
 
@@ -51,35 +64,38 @@ RSpec.describe Interface do
   end
 
   describe "#print_game_result" do
-    def game_word_pattern
-      Regexp.new(Regexp.escape(game.word))
+    context "when the game is won" do
+      before do
+        allow(game).to receive(:won?).and_return(true)
+      end
+
+      it "displays a success message" do
+        expect(view).to receive(:puts).with('Well done!')
+        view.print_game_result
+      end
+
+      it "displays the word" do
+        expect(view).to receive(:puts).with(/TEST/)
+        view.print_game_result
+      end
     end
 
-    it "displays a success message if the game was won" do
-      win_game
-      expect(view).to receive(:puts).with('Well done!')
-      allow(view).to receive(:puts)
-      view.print_game_result
-    end
+    context "when the game is not won" do
+      before do
+        allow(game).to receive(:won?).and_return(false)
+      end
 
-    it "displays the word if the game was won" do
-      win_game
-      allow(view).to receive(:puts)
-      expect(view).to receive(:puts).with(game_word_pattern)
-      view.print_game_result
-    end
+      it "displays a failure message" do
+        expect(view).to receive(:puts).with "Better luck next time ..."
+        allow(view).to receive(:puts)
+        view.print_game_result
+      end
 
-    it "displays a failure message if the game was not won" do
-      expect(view).to receive(:puts).with "Better luck next time ..."
-      allow(view).to receive(:puts)
-      view.print_game_result
-    end
-
-    it "displays the word if the game was not won" do
-      win_game
-      allow(view).to receive(:puts)
-      expect(view).to receive(:puts).with(game_word_pattern)
-      view.print_game_result
+      it "displays the word" do
+        allow(view).to receive(:puts)
+        expect(view).to receive(:puts).with(/TEST/)
+        view.print_game_result
+      end
     end
   end
 
