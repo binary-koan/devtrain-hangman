5042fd4b3e6d34e0abe6449c518817d0473cc158
Jono Mingard <reason.koan@gmail.com>
2015-11-11 13:17:30 +1300

    Fix command-line initialiser with renamed Game

--- init.rb
+++ init.rb
@@ -1,9 +1,9 @@
 require_relative 'lib/controller'
-require_relative 'lib/hangman'
+require_relative 'lib/game'
 require_relative 'lib/interface'
 
 words = File.read('words.txt').split
 
-game = Hangman.new(words.sample)
+game = Game.new(words.sample)
 view = Interface.new(game)
 Controller.new(game, view).play
