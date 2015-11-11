7ce84d06367e5010edb50e8481b6fd1ca6f89c84
Jono Mingard <reason.koan@gmail.com>
2015-11-11 13:17:53 +1300

    Improve code style in game spec

--- lib/game.rb
+++ lib/game.rb
@@ -35,12 +35,6 @@ class Game
   end
 
   def apply_guess(guess)
-    @guessed_letters.add(guess) if valid_guess?(guess)
-  end
-
-  private
-
-  def valid_guess?(guess)
-    guess =~ /\A[A-Z]\z/ && !@guessed_letters.include?(guess)
+    @guessed_letters.add?(guess) if guess =~ /\A[A-Z]\z/
   end
 end
--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -5,91 +5,93 @@ RSpec.describe Game do
 
   let(:game) { Game.new('test') }
 
-  describe '#in_progress?' do
-    it 'should be true for a newly created game' do
-      expect(game).to be_in_progress
+  describe "#in_progress?" do
+    subject { game.in_progress? }
+
+    context "for a newly created game" do
+      it { is_expected.to be true }
     end
 
-    it 'should be false when the game is won' do
-      win_game
-      expect(game).to be_won
-      expect(game).not_to be_in_progress
+    context "when the game is won" do
+      before { win_game }
+      it { is_expected.to be false }
     end
 
-    it 'should be false when the game is lost' do
-      lose_game
-      expect(game).to be_lost
-      expect(game).not_to be_in_progress
+    context "when the game is lost" do
+      before { lose_game }
+      it { is_expected.to be false }
     end
   end
 
-  describe '#guessed?' do
-    it 'should be false for invalid guesses' do
-      game.apply_guess('ABC')
-      expect(game).not_to be_guessed('ABC')
+  describe "#guessed?" do
+    it "is false for invalid guesses" do
+      game.apply_guess("ABC")
+      expect(game).not_to be_guessed("ABC")
     end
 
-    it 'should be false for letters which have not been guessed' do
-      game.apply_guess('B')
-      expect(game).not_to be_guessed('A')
+    it "is false for letters which have not been guessed" do
+      game.apply_guess("B")
+      expect(game).not_to be_guessed("A")
     end
 
-    it 'should be true for letters which have been guessed correctly' do
+    it 'is true for letters which have been guessed correctly' do
       guesses = guess_correctly(1)
       expect(game).to be_guessed(guesses[0])
     end
 
-    it 'should be true for letters which have been guessed incorrectly' do
+    it 'is true for letters which have been guessed incorrectly' do
       guesses = guess_incorrectly(1)
       expect(game).to be_guessed(guesses[0])
     end
   end
 
   describe '#incorrect_guesses' do
-    it 'should initially be an empty' do
-      expect(game.incorrect_guesses).to be_empty
+    subject { game.incorrect_guesses }
+
+    context "for a newly created game" do
+      it { is_expected.to be_empty }
     end
 
-    it 'should remain empty when an invalid character is guessed' do
-      game.apply_guess('1')
-      expect(game.incorrect_guesses).to be_empty
+    context "when an invalid character has been guessed" do
+      before { game.apply_guess('1') }
+      it { is_expected.to be_empty }
     end
 
-    it 'should be populated with incorrect letters' do
-      guess_incorrectly(2)
-      expect(game.incorrect_guesses.size).to eq 2
+    context "when incorrect letters have been guessed" do
+      before { guess_incorrectly(2) }
+      it { is_expected.to have(2).items }
     end
 
-    it 'should not be populated with correct letters' do
-      guess_correctly(2)
-      expect(game.incorrect_guesses.size).to eq 0
+    context "when correct letters have been guessed" do
+      before { guess_correctly(2) }
+      it { is_expected.to have(2).items }
     end
   end
 
-  describe '#apply_guess' do
-    it 'should succeed for letters in the word' do
-      expect(game.apply_guess('T')).to be_truthy
+  describe "#apply_guess" do
+    it "succeeds when guessing a letter from the word" do
+      expect(game.apply_guess("T")).to be_truthy
     end
 
-    it 'should succeed for letters not in the word' do
-      expect(game.apply_guess('A')).to be_truthy
+    it "succeeds when guessing a letter which isn't in the word" do
+      expect(game.apply_guess("A")).to be_truthy
     end
 
-    it 'should fail when guessing the same letter twice' do
-      expect(game.apply_guess('A')).to be_truthy
-      expect(game.apply_guess('A')).to be_falsy
+    it "fails when guessing the same letter twice" do
+      expect(game.apply_guess("A")).to be_truthy
+      expect(game.apply_guess("A")).to be_falsy
     end
 
-    it 'should fail for lowercase letters' do
-      expect(game.apply_guess('a')).to be_falsy
+    it "fails for lowercase letters" do
+      expect(game.apply_guess("a")).to be_falsy
     end
 
-    it 'should fail for strings longer than 1' do
-      expect(game.apply_guess('AB')).to be_falsy
+    it "fails for strings longer than 1" do
+      expect(game.apply_guess("AB")).to be_falsy
     end
 
-    it 'should fail for non-alphabetical characters' do
-      expect(game.apply_guess('!')).to be_falsy
+    it "fails for non-alphabetical characters" do
+      expect(game.apply_guess("!")).to be_falsy
     end
   end
 end
