dd1a326481e7faa78e205257b1e6422e5f3ce7ad
Jono Mingard <reason.koan@gmail.com>
2015-11-24 10:56:36 +1300

    Implement custom jQuery select box

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,7 +12,4 @@
 //
 //= require jquery
 //= require jquery_ujs
-//= require microplugin
-//= require sifter
-//= require selectize
 //= require_tree .
--- app/assets/javascripts/games.js
+++ app/assets/javascripts/games.js
@@ -1,6 +1,18 @@
-// Place all the behaviors and hooks related to the matching controller here.
-// All this logic will automatically be available in application.js.
+jQuery(function($) {
+  function openSelect() {
+    $(this).closest('.letter-select').addClass('open');
+  }
 
-// $(function() {
-//   $('select.form-control').selectize();
-// });
+  function closeSelect() {
+    $(this).closest('.letter-select').removeClass('open');
+  }
+
+  $('.letter-select input[type=text]').focus(openSelect).blur(closeSelect).on('input', function() {
+    if ($(this).val()) closeSelect.call(this);
+    else openSelect.call(this);
+  });
+
+  $('.letter-select button').mousedown(function() {
+    $(this).closest('.letter-select').find('input[type=text]').val($(this).text());
+  });
+});
